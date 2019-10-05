2ba26f2fbaa41d9529dad7249d6bb2220c6bf16e
Adam Newlyn <lord.beaker@gmail.com>
2019-10-05 16:26:34 -0700

    Added static routes

--- app.py
+++ app.py
@@ -13,39 +13,36 @@ con = psycopg2.connect(
     host = "127.0.0.1",
     database = "low_birth_weight",
     user = "postgres",
-    password = "postgres",
+    password = "Sunsh1ne2",
     port="5432"
 )
 
 ################# Load Birth data Table ###################
 #create cursor
 cursor = con.cursor()
-
 #xecute query
 cursor.execute("select location, year, percentage_of_babies from birth_data")
-
 rows = cursor.fetchall()
 for r in rows:
-    print (f"location {r[0]} year {r[1]} percentage_of_babies {r[2]} ")
-
+   print (f"location {r[0]} year {r[1]} percentage_of_babies {r[2]} ")
 # close the cursor
 cursor.close()
 
 ################# Load Aggregate Table ###################
 #create new cursor
 cursor1 = con.cursor()
-
 #execute query
 # cursor1.execute("select location, Min, Max, Mean, ArrayPlus, ArrayMinus from aggregate_data")
 cursor1.execute("select * from aggregate_data")
-
 rows1 = cursor1.fetchall()
+
+print (rows1)
+
 for r1 in rows1:
-    print (f"location {r1[0]} Min {r1[1]} Max {r1[2]} Mean {r1[3]} ArrayPlus {r1[4]} ArrayMinus {r1[5]}")
+   print (f"location {r1[0]} Min {r1[1]} Max {r1[2]} Mean {r1[3]} ArrayPlus {r1[4]} ArrayMinus {r1[5]}")
 
 # close the cursor
 cursor1.close()
-
 #close the connection
 con.close()
 
@@ -55,17 +52,25 @@ file = './static/data/oregon-washignton-counties-geojson.json'
 with open(file, encoding='utf-8-sig') as json_file:
 
     json_data = json.load(json_file)
-    print(json_data)
+    # print(json_data)
 
 
 ################# Flask Routes ###################
 @app.route('/')
 def root():
-    return app.send_static_file('Test_Choropleth.html')
+    return app.send_static_file('index.html')
 
-@app.route('/test')
-def chart():
-    return app.send_static_file('Test_Choropleth2.html')
+@app.route('/choropleth')
+def choropleth():
+    return app.send_static_file('choropleth.html')
+
+@app.route('/line')
+def line():
+    return app.send_static_file('linechart.html')
+
+@app.route('/bar')
+def bar():
+    return app.send_static_file('error-bar.html')
 
 @app.route('/API/CountyGeoJSON')
 def geoJSON():
